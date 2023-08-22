a4b5d2bb1c7a97eb545468af52269feddde54872
rehan012 <rehan24996@gmail.com>
2023-08-22 21:17:07 +0530

    test

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -16,7 +16,7 @@ export const signup = async (req, res, next) => {
         const { password, ...othersData } = newUser._doc;
         res
             .cookie("access_token", token, {
-                secure:false
+                sameSite: 'none'
             })
             .status(200)
             .json(othersData);
@@ -39,7 +39,7 @@ export const signin = async (req, res, next) => {
         const { password, ...othersData } = user._doc;
 
         res
-            .cookie("access_token", token, {httpOnly: false,secure:false})
+            .cookie("access_token", token, {httpOnly: false,sameSite: 'none'})
             .status(200)
             .json(othersData);
     } catch (err) {
