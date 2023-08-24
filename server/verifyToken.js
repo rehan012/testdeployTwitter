4874b8a1f82c51483070e0e2dec0dc06ec4b51fa
rehan012 <rehan24996@gmail.com>
2023-08-24 16:46:12 +0530

    test

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -11,7 +11,7 @@ export const verifyToken = (req, res, next) => {
   if (!token) return next(handleError(401, "You are not authenticated"));
 
   jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) return next(createError(403, "Token is invalid"));
+    if (err) return next(handleError(403, "Token is invalid"));
     req.user = user;
     next();
   });
